2bb3a2106582d466bdad136e7461e93ca9126b88
duzy_pit <piatek.tomasz@gmail.com>
2017-03-28 20:16:04 +0200

    first exp with tree visualization

--- lab/self/bin_tree/bt1.c
+++ lab/self/bin_tree/bt1.c
@@ -1,4 +1,5 @@
-#inculude <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node {
 	int key_value;
@@ -14,7 +15,7 @@ void destroy_tree(struct node *leaf){
 		destroy_tree(leaf->left);
 		destroy_tree(leaf->right);
 		//show yourself!
-		pritnf("I'm gonna be free: %d\n", leaf->key_value);
+		//printf("I'm gonna be free: %d\n", leaf->key_value);
 		free(leaf);
 	}
 }
@@ -27,7 +28,7 @@ void insert(int key, struct node **leaf){
 		*leaf = (struct node *) malloc(sizeof(struct node));
 		(*leaf)->key_value = key;
 		//init the children to null
-		(*leaf)->left = NULL:
+		(*leaf)->left = NULL;
 		(*leaf)->right = NULL;
 	} else if(key < (*leaf)->key_value) {
 		insert(key, &(*leaf)->left);
@@ -44,7 +45,7 @@ struct node *search(int key, struct node *leaf){
 		} else if(key < leaf->key_value) {
 			return search(key, leaf->left);
 		} else {
-			return serch(key, leaf->right);
+			return search(key, leaf->right);
 		}
 	} else {
 		return NULL;
@@ -59,24 +60,86 @@ void print_preorder(struct node *tree){
 	}
 }
 
+void print_simple(int l, struct node *leaf){
+	int i;
 
-void print_inorder(struct node *tree){
-	if(tree){
-		print_inorder(tree->left);
-		printf("%d\n", tree->key_value);
-		print_inorder(tree_right);
+	if(leaf){
+		print_simple(l+1, leaf->right);
+		for(i=0; i<l; ++i){
+			printf(" ");
+		}
+		printf("%d\n",leaf->key_value);
+		print_simple(l+1, leaf->left);
+	}
+}
+
+void padding(char ch, int n){
+	int i;
+	for(i = 0; i < n; i++){
+		putchar(ch);
+	}
+}
+
+void structure(int level, struct node *leaf){
+	
+	if (leaf == NULL){
+		padding('\t', level);
+		puts("-");
+	} else {
+		structure(level+1, leaf->right);
+		padding('\t', level);
+		printf("%d\n",leaf->key_value);
+		structure(level+1, leaf->left);
+	{
+}
+
+/*
+void print_visual(char sp[2], char sn[2], struct node *tree){
+	char s[2];
+	if(tree != NULL){
+		s = sp;
+		if(sn == cr) {
+			s[s.length() - 2] = ' ';
+		}
+		print_visual(s + cp, cr, tree->right);
+		s = s.substr(0, sp.length()-2);
+		printf("%d",tree->key_value);
+
+		s = sp;
+		if(sn == cl) {
+			s[s.length()-2] = ' ';
+		}
+		print_visual(s+cp, cr, tree->left);
 	}
 
 }
 
+char cr[2];
+char cl[2];
+char cp[2];
+*/
 int main (void){
+/*
+	cr = "  ";		 
+	cl = "  ";
+	cp = "  ";
+	cr[0] = 218;
+	cr[1] = 196;
+	cl[0] = 192;
+	cl[1] = 196;
+	cp[0] = 179;
+*/	
 	struct node *root = NULL;
-	struct node *tmp;
 
-	insert(&root, 9);
-	insert(&root, 4);
-	insert(&root, 15)
-//
+	insert(9, &root);
+	insert(4, &root);
+	insert(15, &root);
+	insert(5, &root);
+	insert(3, &root);
+	//print_visual("", "", root);
+	//print_simple(0,root);
+	structure(0, root);
+	destroy_tree(root);
 
 	return 0;
 }
