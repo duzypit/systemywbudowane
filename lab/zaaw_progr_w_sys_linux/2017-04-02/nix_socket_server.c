97f3e94472ce8a876919d598b2d3dba1d15d9f71
duzypit <piatek.tomasz@gmail.com>
2017-04-05 15:28:52 +0200

    minor changes to server

--- lab/zaaw_progr_w_sys_linux/2017-04-02/nix_socket_server.c
+++ lab/zaaw_progr_w_sys_linux/2017-04-02/nix_socket_server.c
@@ -12,10 +12,13 @@
 int main(void){
 
 	int connection_socket, data_socket, result, len;
-	struct sockaddr_un local, remote;
+	struct sockaddr_un local,
+	/*struct sockaddr*/ remote;
 
 	char * buffer = calloc(BUFF_SIZE, sizeof(char));
 	char * myfifo = "/tmp/myfifo";
+
+	puts("Runnin', huh?");
 	//socket(domain, type, protocol) - jeśli protocol == 0, socket wybierze sam na podstawie parametru type
 	connection_socket = socket(AF_UNIX, SOCK_STREAM,0);
 	if (connection_socket == -1){
@@ -39,6 +42,7 @@ int main(void){
 		exit(EXIT_FAILURE);
 	}
 
+	puts("Waiting for connection...");
 	//listen(int sockfd, int backlog) - backlog jak duża może być kolejka oczekujących połączeń
 	result = listen(connection_socket, 20);
 	if(result == -1){
@@ -47,18 +51,24 @@ int main(void){
 	}
 
 	for(;;){
+		
 		//accep zwraca socket, poprzedni cały czas słucha czy nie ma połączeń!
-		len = sizeof(struct sockaddr_un);
+		len = sizeof(struct sockaddr);
 
 		//accept - remote będzie wypełniona struct sockaddr_un z klienta, len wiadomka
-		data_socket = accept(connection_socket, &remote, &len);
+		if(data_socket = accept(connection_socket, &remote, &len) == -1){
+			prerro("accept");
+			exit(EXIT_FAILURE);
+		}
+
+		puts("Connected...");
 
 		for(;;){
 			//read(int socketfd, void *buf, size_t nbytes) - args: deskryptor, char array na content, ilość bajtów do przeczytania
-			result = read(data_socket, buffer, BUFF_SIZE);
+			result = recv(data_socket, buffer, BUFF_SIZE, 0);
 			if(result == -1){
 				perror("read");
-				exit(EXUT_FAILURE);
+				exit(EXIT_FAILURE);
 			}
 			break;
 		}
