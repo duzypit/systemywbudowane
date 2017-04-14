c4e7de9b61dbe39f215019c3b096552069d3ad2e
duzypit <piatek.tomasz@gmail.com>
2017-04-14 12:42:42 +0200

    simple stream server

--- lab/zaaw_progr_w_sys_linux/2017-04-02/ip_server.c
+++ lab/zaaw_progr_w_sys_linux/2017-04-02/ip_server.c
@@ -1,39 +1,123 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <sys/un.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <arpa/inet.h>
 
 #define PEXIT(str) {perror(str);exit(1);}
+#define PCONT(str){perror(str);continue;}
 #define BUFF_SIZE 256
+#define PORT "3490"
+#define BACKLOG 10
+
 char * myfifo = "/tmp/myfifo";	
 
-int server_init(){
-
-	return connection_socket;
+void sigchld_handler(void){
+	//waitpid() might overwrite errno, so we save and restore it:
+	int saved_errno = errno;
+	while(waitpid(-1, NULL, WNOHANG) >0);
+	errno = saved_errno;
 }
 
-s_parse(char *buff_ptr){
-	if (strcmp(*buff_ptr, "list") != 0){
-		
+//get sockaddr, IPv4 or IPv6
+void * get_in_addr(struct sockaddr *sa){
+	if(sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in *)sa)->sin_addr);
 	}
-	
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(void){
 	system("clear");
+	int sockfd, new_fd; //listen on sockfd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	//char buffer[BUFF_SIZE] = {0};
+	//char *buff_ptr = &buffer;
 	
-	struct sockaddr_in sa;
-	if(inet_pton(AF_INET, "192.168.0.100", &(sa.sin_addr)) <= 0){
-		PEXIT("inet_pton");
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my ip
+	
+	if((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0){
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
 	}
 	
-	char buffer[BUFF_SIZE] = {0};
-	char *buff_ptr = &buffer;
-	//http://beej.us/guide/bgnet/output/html/multipage/clientserver.html
+	// loop throught all the results and bind to first we can
+	for(p = servinfo; p != NULL; p = p->ai_next){
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
+			PCONT("server: socket");
+		}
+		
+		if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
+			PEXIT("setscokopt");			
+		}
+		
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+			close(sockfd);
+			PCONT("server: bind");
+		}
+		break;
+	}
+	
+	freeaddrinfo(servinfo); //all done with this structure
+	
+	if(p == NULL){
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+	
+	if (listen(sockfd, BACKLOG) == -1) {
+		PEXIT("listen");
+	}
+	
+	sa.sa_handler = sigchld_handler; //reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1){
+		PEXIT("sigaction");
+	}
+	
+	printf("server: waitning for connections...\n");
+	while(1){ //main accept loop
+		sin_size = sizeof(their_addr);
+		new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
+		if (new_fd == -1) {
+			PCONT("accept");
+		}
+		
+		inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *) &their_addr), s, sizeof(s));
+		printf("server: got connection from %s\n",s);
+		
+		if(!fork()){ //this is the child process
+			close(sockfd); //child doesn't need the listener
+			if(send(new_fd, "hello world!",13, 0) == -1){
+				perror("send");
+				close(new_fd);
+				exit(0);
+			}
+			close(new_fd); // parent doesn't need this
+			return 0;
+		}
+	} //end while(1)
+	
+	
+	
+	
 /*	
 	struct addrinfo {
 		int              ai_flags;     // AI_PASSIVE, AI_CANONNAME, etc.
@@ -62,45 +146,4 @@ int main(void){
 					const struct addrinfo *hints,
 					struct addrinfo **res);	
 	*/
-
-
-	while(strcmp(*buff_ptr, "end") != 0){
-		
-
-		len = sizeof(struct sockaddr);
-
-
-		if((data_socket = accept(connection_socket, &remote, &len)) == -1){
-			PEXIT("accept");
-		}
-
-		puts("Connected...");
-
-		while(strcmp(*buff_ptr, "end") != 0){
-			memset(*buff_ptr, '\0', BUFF_SIZE);
-
-			result = recv(data_socket, *buff_ptr, BUFF_SIZE, 0);
-			if(result == -1){
-				PEXIT("read");
-			}
-			
-			s_parse(*buff_ptr);
-			
-			//printf("%s \n", buffer);
-			sleep(1);			
-			buffer[strcspn(buffer,"\n")] = 0;
-			if(strcmp(buffer, "end") == 0){
-				puts("End detected...");
-			}
-		}
-		
-	}
-
-	
-	close(data_socket);
-	close(connection_socket);
-	unlink(myfifo);
-
-	return 0;
-
 }
