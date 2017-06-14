14397c4fa865065c3a8d9af6f9cd45d8dc32f9df
duzypit <piatek.tomasz@gmail.com>
2017-06-14 09:33:42 +0200

    014 ready

--- obiektowka/hw/014_class6.cpp
+++ obiektowka/hw/014_class6.cpp
@@ -1,61 +1,82 @@
 /*
- * @author Tomasz Piątek
+ * @author Tomasz Piątek, @rikon
 ** @brief Class 7-points Write a class to handle a file with all constructors, destructor, operators... (C++11 recommended)
  */
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <utility>
-//#include <fcntl.h>
-enum file_flags: int {
-	O_RONLY = 1,
-	O_WRONLY = 2,
-	O_APPEND = 4,
-	O_WR_O_APP = 6,
+
+/*
+enum flags @rikon
+*/
+enum flags : int {
+	O_RONLY = 1 << 1,
+	O_WRONLY = 1 << 2,
+	O_APPEND = 1 << 4,
+	O_WRONLY_APPEND = 1 << 6
 };
 
-class File {
-public:
+/*
+overloaded oprerator | for enum flags type @rikon
+*/
+flags operator|(flags lhs, flags rhs){
 	
-	std::ifstream myfile;
-	std::string filename;
-	file_flags flags;
+	return static_cast<flags>(static_cast<int>(lhs) | static_cast<int>(rhs));
+}
 
+/*
+File class
+*/
+class File {
+private:
+	
+	std::ifstream my_file;
+	std::string path;
+	flags file_flags;
+
+public:
 	File(){}
 
-	File(std::string fname, file_flags flag) /*: myfile(fname, std::ios_base::app)*/{
+	File(std::string fname, flags flag) /*: my_file(path, translate_flags(flag))*/{
 		//default ctor
-		this->filename = fname;
-		this->flags = flag;
-		std::cout << "Flags: " << flag << std::endl;
-
-		std::cout << "default ctor" << std::endl;
+		this->path = fname;
+		this->file_flags = flag;
+		open_file();
 	};
 
-	File(const File& rhs)/* : myfile(std::copy(rhs))*/ {
+	File(const File& rhs)/* : my_file(std::copy(rhs))*/ {
 		//copy ctor
 		std::cout << "cpy ctor" << std::endl;
-
+		this->path = rhs.path;
+		this->file_flags = rhs.file_flags;
+		open_file();
 	}
 
-	File& operator=(const File& rhs){
-		//copy assign op
-		std::cout << "cpy assign op" << std::endl;
+	File operator=(const File& rhs){
+		//copy assign op @rikon
 		if(this != &rhs){
-
+			File tmp  {rhs.path, rhs.file_flags};
+			return tmp;
+		} else {
+			return *this;
 		}
-		return *this;
 	}
 
 	~File(){
 		//dstr
-		myfile.close();
+		my_file.close();
 		std::cout << "Dstr: file closed" << std::endl;
 	}
 
-	int translate_flags(file_flags flag){
+	void open_file(){
+		my_file.close();
+		my_file.open(this->path, translate_flags(this->file_flags));
+	}
+
+	std::ios_base::openmode translate_flags(flags flag){
 		switch(flag) {
-			case (O_WRONLY | O_APPEND): return std::ios::app;
+			case (O_WRONLY_APPEND): return std::ios::app;
 			case (O_WRONLY) : return std::ios::out;
 			default : return std::ios::in;
 		}
@@ -64,22 +85,14 @@ public:
 };
 
 
-	/*
-	overloaded oprerator | for enum file_flags type
-	*/
-	file_flags operator|(const file_flags& lhs, const file_flags& rhs){
-		
-		return static_cast<file_flags>(static_cast<char>(lhs) | static_cast<char>(rhs));
-	}
-
 int main() {
     File f {"/tmp/aa", O_WRONLY | O_APPEND}; 
 
-    //File f2 { f } ; 
+    File f2 { f } ; 
 
-    //File f3 = f2; 
+    File f3 = f2; 
 
-    //File f4; 
+    File f4; 
 	
-    //f = f3; 
+    f = f3; 
 }
